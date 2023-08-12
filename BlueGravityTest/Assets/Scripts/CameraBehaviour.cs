82dd2f998d1e6481114499a80ead8d5bb17fcc34
Andrei Pettmant <90349461+AndreiPettmant@users.noreply.github.com>
2023-08-12 12:28:49 -0300

    Dialogue System
    Dialogue system created

---
+++ BlueGravityTest/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    [SerializeField]
+    private float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
